2b0dc049611025f73cf7ee0dd79e5b8c176f6cdb
LiamBender <liam.bendiksen@gmail.com>
2023-10-21 00:27:37 +0200

    Added comments to script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const buttons = document.querySelectorAll('button[id^="button"]');
 const contents = document.querySelectorAll('section[id^="menu"]');
 let buttonClicked = false;
 
-function toggleContent(index) {
+function toggleContent(index) { // Uses the parameter to display the right content depending on button click
   contents.forEach((content) => (content.style.display = "none"));
   contents[index].style.display = "block";
   buttonClicked = true;
 }
 
-buttons.forEach((button, index) => {
+buttons.forEach((button, index) => { // Grabs the selected button's index and uses it as an argument in the toggleContent function
   button.addEventListener("click", () => toggleContent(index));
 });
 
-if (!buttonClicked) {
+if (!buttonClicked) { // When no button is selected then display content [0]
   contents[0].style.display = "block";
 }
